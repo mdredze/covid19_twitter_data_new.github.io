3c7998e254455209407acb698df604e9b9edbcaa
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-08-07 09:19:47 -0400

    Update chart-heatmap.js

--- script/chart-heatmap.js
+++ script/chart-heatmap.js
@@ -1,3 +1,4 @@
+// Create my own color theme
 function am4themes_myTheme(target) {
   if (target instanceof am4core.ColorSet) {
     target.list = [
@@ -10,7 +11,7 @@ function am4themes_myTheme(target) {
 am4core.useTheme(am4themes_myTheme);
 am4core.useTheme(am4themes_animated);
 
- // Create map instance
+// Create map instance
 var chart = am4core.create("heatchartdiv", am4maps.MapChart);
 
 // Set map definition
@@ -70,25 +71,24 @@ polygonTemplate.strokeWidth = 0.5;
 // Start info with US averages
 var info = document.getElementById("info");
 info.innerHTML = "<h3> United States Average </h3>";
-info.innerHTML += "Average mobility before 3/16: " + 65.58009974912021 + "km"
-  + "<br>" + "Average mobility after 3/16: " + 29.531047545190876 + "km"
-  + "<br>" + "Average reduction: " + 54.97 + "%"
-  + "<br>" + "Median reduction: " + 84.95 + "%"
-  + "<br>" + "Number of Twitter users: " + 3898305; 
+info.innerHTML += "Average mobility before 3/16: " + 65.58009974912021 + "km" +
+  "<br>" + "Average mobility after 3/16: " + 29.531047545190876 + "km" +
+  "<br>" + "Average reduction: " + 54.97 + "%" +
+  "<br>" + "Median reduction: " + 84.95 + "%" +
+  "<br>" + "Number of Twitter users: " + 3898305;
 
 // Onclick for states, open its stats
 polygonTemplate.events.on("hit", function(ev) {
   var data = ev.target.dataItem.dataContext;
   var info = document.getElementById("info");
-  info.innerHTML = "<h3>" + data.name + "<br>" + " (" + data.id  + ")</h3>";
+  info.innerHTML = "<h3>" + data.name + "<br>" + " (" + data.id + ")</h3>";
   if (data.id) {
-    info.innerHTML += "Average mobility before 3/16: " + data.mobility_before_distancing + "km"
-    + "<br>" + "Average mobility after 3/16: " + data.mobility_after_distancing + "km"
-    + "<br>" + "Average reduction: " + data.value
-    + "<br>" + "Median reduction: " + data.median_reduction
-    + "<br>" + "Number of Twitter users: " + data.num_users;
-  }
-  else {
+    info.innerHTML += "Average mobility before 3/16: " + data.mobility_before_distancing + "km" +
+      "<br>" + "Average mobility after 3/16: " + data.mobility_after_distancing + "km" +
+      "<br>" + "Average reduction: " + data.value +
+      "<br>" + "Median reduction: " + data.median_reduction +
+      "<br>" + "Number of Twitter users: " + data.num_users;
+  } else {
     info.innerHTML += "<i>No description provided.</i>"
   }
 });
